59e0d2d870fdf2a283480611abaef36b0063534e
shubhamk09 <shubhamkalihari09@gmail.com>
2023-09-20 20:31:45 +0530

    Addeed Account operations for deletion, updation and instertion

--- src/DatabaseOperations/inc/AccountOperations.hpp
+++ src/DatabaseOperations/inc/AccountOperations.hpp
@@ -8,6 +8,9 @@
  * @copyright Copyright (c) 2023
  * 
  */
+#include <sstream> // For std::ostringstream
+#include <iomanip> // For std::hex, std::uppercase, std::setw, and std::setfill
+
 #include "DatabaseOperations.hpp"
 
 namespace Banking
@@ -25,9 +28,18 @@ public:
     AccountOperations& operator=(AccountOperations&&) = default;
 public:
     std::string getAccountBalanceById(std::string &accNumber);
-    std::string getAccountTransactionsById(std::string &accNumber);
+    nlohmann::json getAccountTransactionsById(std::string &accNumber);
     std::string getAccountBranchById(std::string &accNumber);
     bool isActiveAccount(std::string &accNumber);
+    void setAccountBalanceById(std::string &accNumber, int newBalance);
+    void setAccountTransactionById(std::string &accNumber, nlohmann::json &newTransaction);
+    void setAccountStatusById(std::string &accNumber, bool status);
+    void setAccountBranchById(std::string &accNumber, std::string &newBranch);
+    void addAccount(std::string &accNumber, std::string &accBalance, nlohmann::json &accTransaction, 
+                        std::string &accBranch, bool &isActive);
+    void deleteAccount(std::string &accNumber);
+    std::string createHexJson(nlohmann::json &accTransaction);
+    nlohmann::json createTransactionJson(std::string &hexJson, std::string &accNumber);
     
 };  
 
